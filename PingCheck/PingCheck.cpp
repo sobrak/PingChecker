96aee26f7a65bb202c4d8c7fe3ba6a46aa4462e2
SoBraK <99163814+sobrak@users.noreply.github.com>
2022-12-22 16:33:51 +0300

    Add files via upload

---
+++ PingCheck/PingCheck.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    while (true)
+    {
+        int x = system("ping 8.8.8.8 > log.txt");
+        if (x == 0) {
+            cout << "success" << "\n";
+        }
+        else {
+            cout << "failed" << "\n";
+        }
+    }
+}
