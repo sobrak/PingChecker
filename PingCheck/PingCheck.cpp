aa74da6a138d489c6fefd7c7a10ca8a4a0450de4
SoBraK <99163814+sobrak@users.noreply.github.com>
2022-12-25 09:52:13 +0300

    Add files via upload

--- PingCheck/PingCheck.cpp
+++ PingCheck/PingCheck.cpp
@@ -1,14 +1,20 @@
 ﻿#include <iostream>
+#include <ctime>
 using namespace std;
 int main() {
+    time_t now = time(0);
+    tm* localtm = localtime(&now);
     while (true)
     {
         int x = system("ping 8.8.8.8 > log.txt");
         if (x == 0) {
             cout << "success" << "\n";
+            cout << "\n";
         }
         else {
-            cout << "failed" << "\n";
+            cout << "FAILED" << "\n";
+            cout << "Error time : " << asctime(localtm) << "\n";
+            cout << "\n";
         }
     }
 }
