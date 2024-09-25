ec004b6f9a01865cd46275413ca6c686ec5e2a0a
SoBraK <99163814+sobrak@users.noreply.github.com>
2024-09-25 20:34:32 +0300

    Update

--- PingCheck/PingCheck.cpp
+++ PingCheck/PingCheck.cpp
@@ -1,20 +1,33 @@
 ﻿#include <iostream>
 #include <ctime>
+#include <windows.h>
+
 using namespace std;
+
 int main() {
+
     time_t now = time(0);
     tm* localtm = localtime(&now);
+    bool Is_Failed = false;
+
     while (true)
     {
         int x = system("ping 8.8.8.8 > log.txt");
         if (x == 0) {
-            cout << "success" << "\n";
+            system("Color 0A");
+            cout <<  "success" << "\n";
             cout << "\n";
         }
         else {
-            cout << "FAILED" << "\n";
-            cout << "Error time : " << asctime(localtm) << "\n";
-            cout << "\n";
+            if (!Is_Failed)
+            {
+                Is_Failed = true;
+                system("Color 04");
+                cout << "FAILED" << "\n";
+                cout << "Error time : " << asctime(localtm) << "\n";
+                cout << "Waiting for connect..." << "\n";
+                cout << "\n";
+            }
         }
     }
 }
