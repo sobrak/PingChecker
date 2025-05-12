15eede7385665987993b2d325aac90917be4d3f6
SoBraK <99163814+sobrak@users.noreply.github.com>
2025-05-12 21:04:39 +0300

    Update
    fixed bugs

--- PingCheck/PingCheck.cpp
+++ PingCheck/PingCheck.cpp
@@ -2,31 +2,33 @@
 #include <ctime>
 #include <windows.h>
 
-using namespace std;
+time_t now = time(0);
+tm* localtm = localtime(&now);
+int PingResult;
+bool IsFailed = false;
+bool WaitingConnect = false;
 
 int main() {
-
-    time_t now = time(0);
-    tm* localtm = localtime(&now);
-    bool Is_Failed = false;
-
     while (true)
     {
-        int x = system("ping 8.8.8.8 > log.txt");
-        if (x == 0) {
+        PingResult = system("ping 8.8.8.8 > log.txt");
+
+        if (PingResult == 0) {
+            WaitingConnect = false;
             system("Color 0A");
-            cout <<  "success" << "\n";
-            cout << "\n";
+            std::cout <<  "success" << "\n";
+            std::cout << "\n";
         }
         else {
-            if (!Is_Failed)
+            IsFailed = true;
+            if (!WaitingConnect)
             {
-                Is_Failed = true;
+                WaitingConnect = true;
                 system("Color 04");
-                cout << "FAILED" << "\n";
-                cout << "Error time : " << asctime(localtm) << "\n";
-                cout << "Waiting for connect..." << "\n";
-                cout << "\n";
+                std::cout << "FAILED" << "\n";
+                std::cout << "Error time : " << asctime(localtm) << "\n";
+                std::cout << "Waiting for connect..." << "\n";
+                std::cout << "\n";
             }
         }
     }
